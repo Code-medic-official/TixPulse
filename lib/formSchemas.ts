c8a981bbace23f7cf90804373090eee0c26bca5b
Code-medic-official <codemedic2@gmail.com>
2025-03-16 03:15:09 +0300

    Git added Uploadthing intergration

--- lib/formSchemas.ts
+++ lib/formSchemas.ts
@@ -13,13 +13,23 @@ export const onboardingSchema = z.object({
 });
 
 export const eventZodSchema = z.object({
-	name: z.string().min(2).max(700),
-	description: z.string().min(25).max(5000),
+	name: z
+		.string({ required_error: "Event name is required!" })
+		.min(2, "Must be at least 2 characters long!")
+		.max(700),
+	description: z
+		.string({ required_error: "Description is required!" })
+		.min(25, "Must be at least 25 characters long!")
+		.max(5000),
 	slogan: z.string().max(300).optional(),
 	dressCode: z.string().optional(),
-	banner: z.null(),
-	date: z.string(),
-	venue: z.string(),
+
+	banner: z.any(),
+
+	date: z.string({ required_error: "Date is required!" }),
+	venue: z
+		.string({ required_error: "Venue is required!" })
+		.min(2, "Must be at least 2 characters long!"),
 	activities: z.string().optional(),
-	ageLimit: z.number().optional(),
+	ageLimit: z.coerce.number().optional(),
 });
